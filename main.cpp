1a1ff1bd9d0a7c69c225279ea47470b907fc2106
Jacob Funk <jacobstevenfunk@gmail.com>
2023-08-03 16:17:40 -0700

    building window

--- main.cpp
+++ main.cpp
@@ -1,21 +1,38 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
 
-int main() {
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW\n";
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
         return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1200, 800, "Cannon Crusaders", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
     }
 
     glfwTerminate();
-
-    GLuint version = glewInit();
-    if (version != GLEW_OK) {
-        std::cerr << "Failed to initialize GLEW: " << glewGetErrorString(version) << '\n';
-        return -1;
-    }
-
-    std::cout << "Successfully initialized GLFW and GLEW!\n";
     return 0;
 }
