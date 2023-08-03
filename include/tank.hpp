8b7f3379c89ed7eaf7e06ce8c36cc413958ab401
Jacob Funk <jacobstevenfunk@gmail.com>
2023-08-03 16:51:58 -0700

    added declaration of functions that will need to be implemented for tank class

--- include/tank.hpp
+++ include/tank.hpp
@@ -1,16 +1,24 @@
 #ifndef tank_hpp
 #define tank_hpp
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+namespace tank{
 
 class Tank{
 public:
+    Tank(float x, float y, float width, float height);
 
+    void render() const;
 
+    void move(float dx);
 
-
+    void handleIP(GLFWwindow* window);
 
 private:
-
+    float x,y; //for position of tank
+    float width, height; //for sizing tank
 
 };
+}//end of namespace
 #endif
