be3569b9423f9a9415d6e71afbc66287a750dd39
Jacob Funk <jacobstevenfunk@gmail.com>
2023-08-05 20:46:40 -0700

    added function for passing vector and also added header file that will take care of collisiondetection

---
+++ include/collisiondetection.hpp
@@ -0,0 +1,22 @@
+#ifndef collisiondetection_hpp
+#define collisiondetection_hpp
+#include "game.hpp"
+#include "tank.hpp"
+#include "enemy.hpp"
+
+
+class CollisionDetection{
+
+//first function checks whether bullets have made contact with ducks
+//if this function finds collisions, it should update the specific duck object to no longer be active,
+//make decision whether one cannonball should be able to take out multiple ducks objects or not
+//two parameters: duck vector, and tank object which will have access to cannonball vector
+//void Hit_Duck(){}
+//second function checks whether a ducks has reached the end or if duck has made contact with tank
+//if this returns true then we stop the program becuase the user has lost.
+//two parameters: duck vector, and tank object
+//void Hit_Tank(){}
+};
+
+
+#endif
