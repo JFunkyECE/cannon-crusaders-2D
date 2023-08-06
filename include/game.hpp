2d278ec2c8807afb109aef1de19625d3ecfad906
Jacob Funk <jacobstevenfunk@gmail.com>
2023-08-06 14:05:29 -0700

    added swtich statements into many member functions in game to account for different game states. also created menus.hpp header file that will contain vbo and vao information for rendering menus

--- include/game.hpp
+++ include/game.hpp
@@ -18,7 +18,7 @@ public:
         Gameover //game finished, win or lose screen
     };
 
-    Game():current_state(GameState::Playing), my_tank(nullptr){
+    Game():current_state(GameState::Start), my_tank(nullptr){
         /* Initialize the library */
         if (!glfwInit())
             exit(-1);
@@ -38,10 +38,9 @@ public:
             }
         my_tank = new tank::Tank();  // This creates a new tank object, effectively resetting it
         ducks_.reserve(50); //reserve enough space for duck objects
-        //Init();
         ducks_.clear();
         lastshot_time = 0.0f;
-        current_state = GameState::Playing; //change back to start page eventually 
+        current_state = GameState::Start; //change back to start page eventually 
         lastspawn_time = 0.0f; 
         spawn_interval = 1.5f; // to make sure enemies dont spawn in too quick succession
         enemies_defeated = 0; //records # of shots fired
@@ -63,62 +62,113 @@ public:
         // Reset other game variables
         ducks_.clear();
         lastshot_time = 0.0f;
-        current_state = GameState::Playing; //change back to start page eventually 
+        current_state = GameState::Start; //change back to start page eventually 
         lastspawn_time = 0.0f; 
         spawn_interval = 1.5f; // to make sure enemies dont spawn in too quick succession
         enemies_defeated = 0; //records # of shots fired
         enemies_on_screen = 0;
     }
 
-    //processes keyboard inputs
+    //processes keyboard inputs depending on what state the game is in
     void ProcessInput(){
-        if (glfwGetKey(window_, GLFW_KEY_LEFT) == GLFW_PRESS) {
-            my_tank->move(-0.04f);  // Move the tank to the left
+        switch (current_state) {
+
+            case GameState::Start:
+                if (glfwGetKey(window_, GLFW_KEY_ENTER) == GLFW_PRESS) {
+                    Init(); //ensures data resets    
+                    current_state = GameState::Playing;
+                }
+                break;
+
+            case GameState::Playing:
+                if (glfwGetKey(window_, GLFW_KEY_LEFT) == GLFW_PRESS) {
+                    my_tank->move(-0.04f);  // Move the tank to the left
+                }
+                if (glfwGetKey(window_, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+                    my_tank->move(0.04f);  // Move the tank to the right
+                }
+                if (glfwGetKey(window_, GLFW_KEY_SPACE) == GLFW_PRESS) {
+                    float current_time = glfwGetTime(); //gives time in seconds
+                    if(current_time - lastshot_time >= 0.2f){ //limit shooting to one per 0.2 seconds or 5 shots per second
+                        my_tank->shoot();
+                        lastshot_time = current_time;
+                    }
+                }
+                if (glfwGetKey(window_, GLFW_KEY_ESC) == GLFW_PRESS) {
+                    current_state = GameState::Paused; // Pause the game
+                }
+                break;
+
+            case GameState::Paused:
+                if (glfwGetKey(window_, GLFW_KEY_ENTER) == GLFW_PRESS) {
+                    current_state = GameState::Playing; 
+                }
+                if (glfwGetKey(window_, GLFW_KEY_ESC) == GLFW_PRESS) {
+                    current_state = GameState::Start; // Resume the game
+                }
+                break;
+
+            case GameState::Gameover:
+                if (glfwGetKey(window_, GLFW_KEY_R) == GLFW_PRESS) {
+                    Restart();
+                }
+                break;
         }
-        if (glfwGetKey(window_, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-            my_tank->move(0.04f);  // Move the tank to the right
-        }
-        if (glfwGetKey(window_, GLFW_KEY_SPACE) == GLFW_PRESS) {
-            float current_time = glfwGetTime(); //gives time in seconds
-            if(current_time - lastshot_time >= 0.2f){ //limit shooting to one per 0.2 seconds or 5 shots per second
-                my_tank->shoot();
-                lastshot_time = current_time;
-            }
-        }
-        if (glfwGetKey(window_, GLFW_KEY_R) == GLFW_PRESS) {
-           Restart();
-        }
+    }
+        
 
     // add input for pausing game
-    }
+
     void Update(){
         
         ProcessInput();
-        spawnEnemy();
         switch (current_state) {
             case GameState::Start:
-                // Maybe display a start screen or instructions
+                //implement startmenu function that displays start menu info
+                //menu::startmenu();
                 break;
             case GameState::Playing:
                 // Normal game update logic
+                spawnEnemy();
                 my_tank->updateCannonballs();
                 updateEnemies();
                 break;
             case GameState::Paused:
                 // Display a pause menu
+                //menu::pausemenu();
                 break;
             case GameState::Gameover:
                 // Display game over screen
+                //menu::gameover()
                 break;
         }
     }
     void Render(){
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(0.0f, 0.5f, 0.0f, 1.0f); // Set clear color to green
-       
-        my_tank->render();
-        my_tank->renderCannonballs();
-        renderEnemies();
+        switch (current_state) {
+            case GameState::Start:
+               glClear(GL_COLOR_BUFFER_BIT);
+               glClearColor(0.0f, 0.5f, 0.0f, 1.0f); // Set clear color to green
+                break;
+            case GameState::Playing:
+               glClear(GL_COLOR_BUFFER_BIT);
+               glClearColor(0.0f, 0.5f, 0.0f, 1.0f); // Set clear color to green
+            
+               my_tank->render();
+               my_tank->renderCannonballs();
+               renderEnemies();
+                break;
+            case GameState::Paused:
+               glClear(GL_COLOR_BUFFER_BIT);
+               glClearColor(0.0f, 0.5f, 0.0f, 1.0f); // Set clear color to green
+                break;
+            case GameState::Gameover:
+               glClear(GL_COLOR_BUFFER_BIT);
+               glClearColor(0.0f, 0.5f, 0.0f, 1.0f); // Set clear color to green
+                break;
+        }
+        
+
+        //add cases for different playing states
     }
 
     GLFWwindow* getWindow() const {
@@ -127,6 +177,7 @@ public:
 
     void Restart(){
         Init();
+        current_state = GameState::Playing;
     } //for when player wants to restart game
 
 
@@ -173,7 +224,7 @@ float lastshot_time;
 float lastspawn_time; //makes sure enemies spawn in reasonable time of one another
 float spawn_interval; // to make sure enemies dont spawn in too quick succession
 std::vector<Ducks::Enemy> ducks_; //stores cannonball data
-int enemies_defeated; //records # of shots fired
+int enemies_defeated; //for completion of game purpose
 int enemies_on_screen;
 };
 
