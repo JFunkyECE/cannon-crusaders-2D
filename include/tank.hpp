2c1bb97f6c77115639965ad010c768d410c688ce
Jacob Funk <jacobstevenfunk@gmail.com>
2023-08-06 15:14:27 -0700

    added more print statement information

--- include/tank.hpp
+++ include/tank.hpp
@@ -84,7 +84,10 @@ public:
         }
     //called when the user presses a or d to move 
     void move(float dx){
-        std::cout << "Xo is " << x0 <<std::endl;
+        std::cout << "Xo is " << x0 << 
+                    ". After moving it would be "
+                    << x0 + dx  <<std::endl;
+        
         if(x0 + dx > -1.0f && x0 + dx < 1.0f - width_){
             x0 += dx;
             x1 += dx;
