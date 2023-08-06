8a15ad038f7768dd5d0c9170239fc369a91f65ad
Jacob Funk <jacobstevenfunk@gmail.com>
2023-08-06 14:39:31 -0700

    movement speed was impacting reach boundaries

--- include/game.hpp
+++ include/game.hpp
@@ -82,10 +82,10 @@ public:
 
             case GameState::Playing:
                 if (glfwGetKey(window_, GLFW_KEY_LEFT) == GLFW_PRESS) {
-                    my_tank->move(-0.04f);  // Move the tank to the left
+                    my_tank->move(-0.05f);  // Move the tank to the left
                 }
                 if (glfwGetKey(window_, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-                    my_tank->move(0.04f);  // Move the tank to the right
+                    my_tank->move(0.05f);  // Move the tank to the right
                 }
                 if (glfwGetKey(window_, GLFW_KEY_SPACE) == GLFW_PRESS) {
                     float current_time = glfwGetTime(); //gives time in seconds
