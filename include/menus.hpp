34d28d0982d3f8b63f8283124831df1dd9f3ddbf
Jacob Funk <jacobstevenfunk@gmail.com>
2023-08-08 21:01:19 -0700

    made start screen

--- include/menus.hpp
+++ include/menus.hpp
@@ -38,11 +38,96 @@ public:
         AddCharToVec('R', x_ + .33f, y_ - 0.3f);
         AddCharToVec('T', x_ + .44f, y_ - 0.3f);
         AddCharToVec('-', x_ + .55f, y_ - 0.3f);
-        AddCharToVec('E', x_ + .66f, y_ - 0.3f);
-        AddCharToVec('N', x_ + .77f, y_ - 0.3f);
-        AddCharToVec('T', x_ + .88f, y_ - 0.3f);
-        AddCharToVec('E', x_ + .99f, y_ - 0.3f);
-        AddCharToVec('R', x_ + 1.1f, y_ - 0.3f);
+        AddCharToVec('P', x_ + .66f, y_ - 0.3f);
+        AddCharToVec('R', x_ + .77f, y_ - 0.3f);
+        AddCharToVec('E', x_ + .88f, y_ - 0.3f);
+        AddCharToVec('S', x_ + .99f, y_ - 0.3f);
+        AddCharToVec('S', x_ + 1.1f, y_ - 0.3f);
+        AddCharToVec('E', x_ + 1.21f, y_ - 0.3f);
+        AddCharToVec('N', x_ + 1.32f, y_ - 0.3f);
+        AddCharToVec('T', x_ + 1.43f, y_ - 0.3f);
+        AddCharToVec('E', x_ + 1.54f, y_ - 0.3f);
+        AddCharToVec('R', x_ + 1.65f, y_ - 0.3f);
+
+        AddCharToVec('Q', x_ , y_ - 0.45f);
+        AddCharToVec('U', x_ + .11f, y_ - 0.45f);
+        AddCharToVec('I', x_ + .22f, y_ - 0.45f);
+        AddCharToVec('T', x_ + .33f, y_ - 0.45f);
+        AddCharToVec('-', x_ + .44f, y_ - 0.45f);
+        AddCharToVec('E', x_ + .55f, y_ - 0.45f);
+        AddCharToVec('S', x_ + .66f, y_ - 0.45f);
+        AddCharToVec('C', x_ + .77f, y_ - 0.45f);
+        
+
+        AddCharToVec('I', x_ , y_ - 0.7f);
+        AddCharToVec('N', x_ + .11f, y_ - 0.7f);
+        AddCharToVec('-', x_ + .22f, y_ - 0.7f);
+        AddCharToVec('G', x_ + .33f, y_ - 0.7f);
+        AddCharToVec('A', x_ + .44f, y_ - 0.7f);
+        AddCharToVec('M', x_ + .55f, y_ - 0.7f);
+        AddCharToVec('E', x_ + .66f, y_ - 0.7f);
+        AddCharToVec('C', x_ + .88f, y_ - 0.7f);
+        AddCharToVec('O', x_ + .99f, y_ - 0.7f);
+        AddCharToVec('N', x_ + 1.1f, y_ - 0.7f);
+        AddCharToVec('T', x_ + 1.21f, y_ - 0.7f);
+        AddCharToVec('R', x_ + 1.32f, y_ - 0.7f);
+        AddCharToVec('O', x_ + 1.43f, y_ - 0.7f);
+        AddCharToVec('L', x_ + 1.54f, y_ - 0.7f);
+        AddCharToVec('S', x_ + 1.65f, y_ - 0.7f);
+        
+        //left arrow - MOVE LEFT
+        AddCharToVec('<', x_ , y_ - 0.85f);
+        AddCharToVec('-', x_ + .1f, y_ - 0.85f);
+        AddCharToVec('M', x_ + .21f, y_ - 0.85f);
+        AddCharToVec('O', x_ + .32f, y_ - 0.85f);
+        AddCharToVec('V', x_ + .43f, y_ - 0.85f);
+        AddCharToVec('E', x_ + .54f, y_ - 0.85f);
+        AddCharToVec('L', x_ + .76f, y_ - 0.85f);
+        AddCharToVec('E', x_ + .87f, y_ - 0.85f);
+        AddCharToVec('F', x_ + .98f, y_ - 0.85f);
+        AddCharToVec('T', x_ + 1.09f, y_ - 0.85f);
+
+        //right arrow - MOVE RIGHT
+        AddCharToVec('-', x_ , y_ - 1.0f);
+        AddCharToVec('>', x_ + .1f, y_ - 1.0f);
+        AddCharToVec('M', x_ + .21f, y_ - 1.0f);
+        AddCharToVec('O', x_ + .32f, y_ - 1.0f);
+        AddCharToVec('V', x_ + .43f, y_ - 1.0f);
+        AddCharToVec('E', x_ + .54f, y_ - 1.0f);
+        AddCharToVec('R', x_ + .76f, y_ - 1.0f);
+        AddCharToVec('I', x_ + .87f, y_ - 1.0f);
+        AddCharToVec('G', x_ + .98f, y_ - 1.0f);
+        AddCharToVec('H', x_ + 1.09f, y_ - 1.0f);
+        AddCharToVec('T', x_ + 1.20f, y_ - 1.0f);
+
+        //SPACE - SHOOT
+        AddCharToVec('S', x_ , y_ - 1.15f);
+        AddCharToVec('P', x_ + .11f, y_ - 1.15f);
+        AddCharToVec('A', x_ + .22f, y_ - 1.15f);
+        AddCharToVec('C', x_ + .33f, y_ - 1.15f);
+        AddCharToVec('E', x_ + .44f, y_ - 1.15f);
+        AddCharToVec('-', x_ + .55f, y_ - 1.15f);
+        AddCharToVec('S', x_ + .66f, y_ - 1.15f);
+        AddCharToVec('H', x_ + .77f, y_ - 1.15f);
+        AddCharToVec('O', x_ + .88f, y_ - 1.15f);
+        AddCharToVec('O', x_ + .99f, y_ - 1.15f);
+        AddCharToVec('T', x_ + 1.1f, y_ - 1.15f);
+        //P - PAUSE
+        AddCharToVec('P', x_ , y_ - 1.3f);
+        AddCharToVec('-', x_ + .11f, y_ - 1.3f);
+        AddCharToVec('P', x_ + .22f, y_ - 1.3f);
+        AddCharToVec('A', x_ + .33f, y_ - 1.3f);
+        AddCharToVec('U', x_ + .44f, y_ - 1.3f);
+        AddCharToVec('S', x_ + .55f, y_ - 1.3f);
+        AddCharToVec('E', x_ + .77f, y_ - 1.3f);
+        AddCharToVec('G', x_ + .88f, y_ - 1.3f);
+        AddCharToVec('A', x_ + .99f, y_ - 1.3f);
+        AddCharToVec('M', x_ + 1.1f, y_ - 1.3f);
+        AddCharToVec('E', x_ + 1.21f, y_ - 1.3f);
+        
+        
+        }
+
         glGenVertexArrays(1, &VAO_);
         glBindVertexArray(VAO_);
 
@@ -410,6 +495,17 @@ public:
                 vertices_.push_back(x + 0.075f); vertices_.push_back(y - 0.1f);
                 vertices_.push_back(x + 0.1f); vertices_.push_back(y - 0.1f); //right side
                 break;
+            case 'V':
+                vertices_.push_back(x); vertices_.push_back(y);
+                vertices_.push_back(x + 0.025f); vertices_.push_back(y);
+                vertices_.push_back(x + 0.0625f); vertices_.push_back(y - 0.1f);
+                vertices_.push_back(x + 0.0375f); vertices_.push_back(y - 0.1f); //left
+
+                vertices_.push_back(x + 0.1f); vertices_.push_back(y);
+                vertices_.push_back(x + 0.075f); vertices_.push_back(y);
+                vertices_.push_back(x + 0.0375f); vertices_.push_back(y - 0.1f);
+                vertices_.push_back(x + 0.0625f); vertices_.push_back(y - 0.1f); //right side
+                break;
             case 'Y': 
                 vertices_.push_back(x); vertices_.push_back(y);
                 vertices_.push_back(x + 0.025f); vertices_.push_back(y);
@@ -436,6 +532,30 @@ public:
                 vertices_.push_back(x + 0.1f); vertices_.push_back(y - 0.05f);
                 vertices_.push_back(x + 0.1f); vertices_.push_back(y - 0.075f);
                 vertices_.push_back(x); vertices_.push_back(y - 0.075f); //middle of A
+                break;
+            case '<':
+                //middle
+                vertices_.push_back(x + 0.05f); vertices_.push_back(y - 0.0375f);
+                vertices_.push_back(x + 0.1f); vertices_.push_back(y - 0.0375f);
+                vertices_.push_back(x + 0.1f); vertices_.push_back(y - 0.0625f);
+                vertices_.push_back(x + 0.05f); vertices_.push_back(y - 0.0625f); //middle
+
+                vertices_.push_back(x + 0.05f); vertices_.push_back(y);
+                vertices_.push_back(x + 0.05f); vertices_.push_back(y - 0.1f);
+                vertices_.push_back(x ); vertices_.push_back(y - 0.05f);
+                vertices_.push_back(x + 0.05f); vertices_.push_back(y);
+
+                break;
+            case '>':
+                vertices_.push_back(x + 0.05f); vertices_.push_back(y - 0.0375f);
+                vertices_.push_back(x); vertices_.push_back(y - 0.0375f);
+                vertices_.push_back(x); vertices_.push_back(y - 0.0625f);
+                vertices_.push_back(x + 0.05f); vertices_.push_back(y - 0.0625f); //middle
+
+                vertices_.push_back(x + 0.05f); vertices_.push_back(y);
+                vertices_.push_back(x + 0.05f); vertices_.push_back(y - 0.1f);
+                vertices_.push_back(x + 0.1f); vertices_.push_back(y - 0.05f);
+                vertices_.push_back(x + 0.05f); vertices_.push_back(y);
                 break;
         }
     }
