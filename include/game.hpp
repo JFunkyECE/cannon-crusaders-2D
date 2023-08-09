f9f5c3c0bff8934f2c3733d1400b4bd11c47ed82
Jacob Funk <jacobstevenfunk@gmail.com>
2023-08-08 22:23:18 -0700

    made enemies spawn more often

--- include/game.hpp
+++ include/game.hpp
@@ -71,7 +71,7 @@ public:
         lastshot_time = 0.0f;
         current_state = GameState::Start; //change back to start page eventually 
         lastspawn_time = 0.0f; 
-        spawn_interval = 1.5f; // to make sure enemies dont spawn in too quick succession
+        spawn_interval = 1.0f; // to make sure enemies dont spawn in too quick succession
         enemies_defeated = 0; //records # of shots fired
         enemies_on_screen = 0;
         game_over = false;
