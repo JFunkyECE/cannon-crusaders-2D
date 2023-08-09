0ea64e377ac577a34f092d64b649b35a8d5b4d98
Jacob Funk <jacobstevenfunk@gmail.com>
2023-08-08 17:35:12 -0700

    added A to menu

--- include/menus.hpp
+++ include/menus.hpp
@@ -14,6 +14,7 @@ public:
 
     Menu(): x_(-1.0f), y_(1.0f), currentstate_(0){  
         float Vertices[] = {
+                //C now
                 x_, y_,
                 x_ + 0.1f, y_,
                 x_ + 0.1f, y_ - 0.025f,
@@ -27,7 +28,28 @@ public:
                 x_ , y_ - 0.075f,
                 x_ + 0.1f, y_ - 0.075f,
                 x_ + 0.1f, y_ - 0.1f,   
-                x_, y_ - 0.1f              
+                x_, y_ - 0.1f, 
+
+                //A now
+                x_ + .11f, y_,
+                x_ + 0.21f, y_,
+                x_ + 0.21f, y_ - 0.025f,
+                x_ + 0.11f, y_ - 0.025f, //top of A
+
+                x_ + 0.11f, y_, 
+                x_ + 0.135f, y_,
+                x_ + 0.135f, y_ - 0.1f,
+                x_ , y_ - 0.1f,         //left side of A
+
+                x_ + 0.11f, y_ - 0.05f,
+                x_ + 0.21f, y_ - 0.05f,
+                x_ + 0.21f, y_ - 0.075f,   
+                x_ + 0.11f, y_ - 0.075f,//middle if A           
+
+                x_ + 0.185f, y_, 
+                x_ + 0.21f, y_,
+                x_ + 0.21f, y_ - 0.1f,
+                x_ + 0.185f, y_ - 0.1f,         //right side of A
         };
         glGenVertexArrays(1, &VAO_);
         glBindVertexArray(VAO_);
@@ -56,7 +78,7 @@ public:
         glBindVertexArray(VAO_);
         glBindBuffer(GL_ARRAY_BUFFER, VBO_);
         glColor3f(1.0f,1.0f,1.0f);
-        for(int i = 0; i < 3; ++i){
+        for(int i = 0; i < 7; ++i){
         glDrawArrays(GL_QUADS,i*4,4);
         }
         }
