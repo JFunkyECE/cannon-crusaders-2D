baa32803c9786891d7de4d64f180f34e513c56d3
Jacob Funk <jacobstevenfunk@gmail.com>
2023-08-08 18:49:32 -0700

    changed structure of menu, will now use lookup function to add characters to vector

--- include/menus.hpp
+++ include/menus.hpp
@@ -4,6 +4,7 @@
 #include <GLFW/glfw3.h>
 #include "game.hpp"
 #include <iostream>
+#include <vector>
 
 namespace menu{
 
@@ -12,55 +13,20 @@ public:
 //purpose of this class is to contain graphic rendering for
 //start menu, pause menu, and end screen 
 
-    Menu(): x_(-1.0f), y_(1.0f), currentstate_(0){  
-        float Vertices[] = {
-                //C now
-                x_, y_,
-                x_ + 0.1f, y_,
-                x_ + 0.1f, y_ - 0.025f,
-                x_, y_ - 0.025f,
+    Menu() : x_(-1.0f), y_(1.0f), currentstate_(0){  
+        vertices_.reserve(1000); //will never reach end
 
-                x_, y_,
-                x_ + 0.025f, y_,
-                x_ + 0.025f, y_ - 0.1f,
-                x_ , y_ - 0.1f,
-
-                x_ , y_ - 0.075f,
-                x_ + 0.1f, y_ - 0.075f,
-                x_ + 0.1f, y_ - 0.1f,   
-                x_, y_ - 0.1f, 
-
-                //A now
-                x_ + .11f, y_,
-                x_ + 0.21f, y_,
-                x_ + 0.21f, y_ - 0.025f,
-                x_ + 0.11f, y_ - 0.025f, //top of A
-
-                x_ + 0.11f, y_, 
-                x_ + 0.135f, y_,
-                x_ + 0.135f, y_ - 0.1f,
-                x_ + 0.11f, y_ - 0.1f,         //left side of A
-
-                x_ + 0.11f, y_ - 0.05f,
-                x_ + 0.21f, y_ - 0.05f,
-                x_ + 0.21f, y_ - 0.075f,   
-                x_ + 0.11f, y_ - 0.075f,//middle if A           
-
-                x_ + 0.185f, y_, 
-                x_ + 0.21f, y_,
-                x_ + 0.21f, y_ - 0.1f,
-                x_ + 0.185f, y_ - 0.1f,         //right side of A
-        };
+        AddToVec('C', x_, y_);
+        
         glGenVertexArrays(1, &VAO_);
         glBindVertexArray(VAO_);
 
         glGenBuffers(1, &VBO_);
         glBindBuffer(GL_ARRAY_BUFFER, VBO_);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices_.size(), vertices_.data(), GL_STATIC_DRAW);
 
         glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
-
     }
 
     ~Menu(){
@@ -68,19 +34,43 @@ public:
         glDeleteVertexArrays(1, &VAO_);
     }
 
-    void updateMenu(int i){
+    void updateMenu(int i, int enemies_defeated){
         currentstate_ = i;
         //called to alter vertices depending on currentstate_
+
+        
     }
 
     void renderMenu(){
-        if(currentstate_ == 0){        
+          
         glBindVertexArray(VAO_);
         glBindBuffer(GL_ARRAY_BUFFER, VBO_);
         glColor3f(1.0f,1.0f,1.0f);
-        for(int i = 0; i < 7; ++i){
+        int number_quads = vertices_.size() / 8; //each quad has 4 2 point coords
+        for(int i = 0; i < number_quads; ++i){
         glDrawArrays(GL_QUADS,i*4,4);
         }
+    }   
+    //adds char to vector
+    void AddToVec(char letter, float x, float y){
+        switch(letter){
+            case 'C':
+
+                vertices_.push_back(x); vertices_.push_back(y);
+                vertices_.push_back(x + 0.1f); vertices_.push_back(y);
+                vertices_.push_back(x + 0.1f); vertices_.push_back(y - 0.025f);
+                vertices_.push_back(x); vertices_.push_back(y - 0.025f);
+
+                vertices_.push_back(x); vertices_.push_back(y);
+                vertices_.push_back(x + 0.025f); vertices_.push_back(y);
+                vertices_.push_back(x + 0.025f); vertices_.push_back(y - 0.1f);
+                vertices_.push_back(x); vertices_.push_back(y - 0.1f);
+
+                vertices_.push_back(x); vertices_.push_back(y - 0.075f);
+                vertices_.push_back(x + 0.1f); vertices_.push_back(y - 0.075f);
+                vertices_.push_back(x + 0.1f); vertices_.push_back(y - 0.1f);
+                vertices_.push_back(x); vertices_.push_back(y - 0.1f);
+                break;
         }
     }
 
@@ -89,6 +79,7 @@ private:
 GLuint VAO_, VBO_;
 float x_,y_;
 int currentstate_;
+std::vector<float> vertices_;
 };
 
 }
