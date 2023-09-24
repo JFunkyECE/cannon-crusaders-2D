7dee28b37c1a57f7ffc1b5cef14eb8b5b202663c
Jacob Funk <jacobstevenfunk@gmail.com>
2023-09-23 22:07:08 -0700

    fixed to 60fps

--- app/main.cpp
+++ app/main.cpp
@@ -1,17 +1,29 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "game.hpp"
+#include <iostream>
+#include <chrono>
 
 int main(void){
+
     // Create Game object
     Game game;
 
-    //Application Loop
+    auto lastTime = std::chrono::high_resolution_clock::now();
+    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(game.getWindow()))
     {
-        game.Update();
+        
+        auto now = std::chrono::high_resolution_clock::now();
+        if(std::chrono::duration_cast<std::chrono::milliseconds>(now - lastTime).count() >= 16){
+            game.Update();
+            lastTime = now;
+        }
         game.Render();
+        /* Swap front and back buffers */
         glfwSwapBuffers(game.getWindow());
+
+        /* Poll for and process events */
         glfwPollEvents();
     }
     return 0;
